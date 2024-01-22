89b44ada6c2608e1ffe04c32d0325b10feaf236c
Vojtech Cerveny <cervik49@gmail.com>
2024-01-22 22:50:35 +0100

    fix: dark mode - select + navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className="w-full" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen w-full min-w-full px-4 py-4 pb-10 sm:px-6 lg:px-8 dark:bg-zinc-900 dark:text-zinc-400",
+          "min-h-screen w-full min-w-full px-4 py-4 pb-10 sm:px-6 md:max-w-2xl lg:px-8 dark:bg-zinc-900 dark:text-zinc-400",
           myFont.className,
           GeistSans.variable,
         )}
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <ModeToggle />
           </div>
           <Link href="/">
-            <h1 className="mx-auto w-full scroll-m-20 pb-4 text-5xl font-black tracking-tight md:max-w-2xl lg:text-5xl">
+            <h1 className="mx-auto w-full max-w-2xl scroll-m-20 pb-4 text-5xl font-black tracking-tight lg:text-5xl">
               Excessus101
             </h1>
           </Link>
