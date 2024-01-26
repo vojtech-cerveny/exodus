9d260f290a7bbf0c10cbd8d4ca3805c09e81a209
Vojtěch Červený <12935892+vojtech-cerveny@users.noreply.github.com>
2024-01-26 11:17:17 +0100

    feat: :passport_control: next-auth + prisma (#1)

---
+++ src/app/components/auth-components.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { signIn, signOut } from "../../../auth";
+
+export function SignIn({ provider, ...props }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn(provider);
+      }}
+    >
+      <Button {...props}>Sign In</Button>
+    </form>
+  );
+}
+
+export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <Button variant="ghost" className="w-full p-0" {...props}>
+        Sign Out
+      </Button>
+    </form>
+  );
+}
