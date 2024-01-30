e0ff9e37398cba1b91ae28e1fc4314026ddab034
Vojtech Cerveny <cervik49@gmail.com>
2024-01-30 15:22:20 +0100

    feat: pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "Exodus90",
+      exec_mode: "cluster",
+      instances: "max", // Or a number of instances
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      env_local: {
+        APP_ENV: "local", // APP_ENV=local
+      },
+      env_dev: {
+        APP_ENV: "dev", // APP_ENV=dev
+      },
+      env_prod: {
+        APP_ENV: "prod", // APP_ENV=prod
+      },
+    },
+  ],
+};
