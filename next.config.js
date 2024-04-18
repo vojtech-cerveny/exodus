710b8dc70c6a50cbaea94b77b3ea59cd1b2072a3
Vojtech Cerveny <cervik49@gmail.com>
2024-04-18 21:39:48 +0200

    feat: docker

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withMDX = require("@next/mdx")();
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  output: "standalone",
   // Optionally, add any other Next.js config below
 };
 
