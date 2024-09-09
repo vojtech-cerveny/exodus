079b7e9cec9ab2f5d24c2737fcf0b6978aa1e03f
Vojtech Cerveny <cervik49@gmail.com>
2024-09-09 11:17:20 +0200

    navigation: change to Exodus90

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -29,7 +29,7 @@ export default function Navigation() {
     <NavigationMenu>
       <NavigationMenuList className="flex flex-wrap">
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Exodus</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Exodus90</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
               <li className="row-span-4">
