b61478941b9c62831ac1134650a35db3b2ea03ae
Vojtech Cerveny <cervik49@gmail.com>
2024-10-17 10:21:11 +0200

    fix: order link -> button in download section

--- src/components/download-text-files.tsx
+++ src/components/download-text-files.tsx
@@ -5,44 +5,44 @@ import { Button } from "./ui/button";
 export function DownloadTextFiles() {
   return (
     <div className="mt-4 flex flex-col gap-2">
-      <Button>
-        <Link
-          className="plausible-event-name=exodus-90-download-pdf flex items-center"
-          download
-          prefetch={false}
-          target="_blank"
-          href="/files/exodus90.pdf"
-        >
+      <Link
+        className="plausible-event-name=exodus-90-download-pdf flex items-center"
+        download
+        prefetch={false}
+        target="_blank"
+        href="/files/exodus90.pdf"
+      >
+        <Button>
           <FileTextIcon className="mr-2" />
           <span>PDF textů pro tisk</span>
-        </Link>
-      </Button>
+        </Button>
+      </Link>
 
-      <Button>
-        <Link
-          className="plausible-event-name=exodus-90-download-epub flex items-center"
-          download
-          prefetch={false}
-          target="_blank"
-          href="/books/exodus90-denni-texty.epub"
-        >
+      <Link
+        className="plausible-event-name=exodus-90-download-epub flex items-center"
+        download
+        prefetch={false}
+        target="_blank"
+        href="/books/exodus90-denni-texty.epub"
+      >
+        <Button>
           <FileTextIcon className="mr-2" />
           <span>Denní texty pro čtečky (epub)</span>
-        </Link>
-      </Button>
+        </Button>
+      </Link>
 
-      <Button>
-        <Link
-          className="plausible-event-name=exodus-90-download-mobi flex items-center"
-          download
-          prefetch={false}
-          target="_blank"
-          href="/files/exodus90-denni-texty.mobi"
-        >
+      <Link
+        className="plausible-event-name=exodus-90-download-mobi flex items-center"
+        download
+        prefetch={false}
+        target="_blank"
+        href="/files/exodus90-denni-texty.mobi"
+      >
+        <Button>
           <FileTextIcon className="mr-2" />
           <span>Denní texty pro čtečky (mobi)</span>
-        </Link>
-      </Button>
+        </Button>
+      </Link>
     </div>
   );
 }
