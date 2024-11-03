211ca8a1c078f3bccc0df1e62c5577c5ba77da12
Vojtech Cerveny <cervik49@gmail.com>
2024-11-03 13:06:58 +0100

    feat: adding sentry session replay for better debugging

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -9,6 +9,10 @@ Sentry.init({
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
