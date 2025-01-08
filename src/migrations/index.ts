cef70ea70f0a2c942fac70519188e76bcbbc2163
Vojtech Cerveny <cervik49@gmail.com>
2025-01-08 19:30:57 +0100

    fix: adding migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20241216_135120 from "./20241216_135120";
+import * as migration_20250108_182948 from "./20250108_182948";
 
 export const migrations = [
   {
     up: migration_20241216_135120.up,
     down: migration_20241216_135120.down,
     name: "20241216_135120",
+  },
+  {
+    up: migration_20250108_182948.up,
+    down: migration_20250108_182948.down,
+    name: "20250108_182948",
   },
 ];
