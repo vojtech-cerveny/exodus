224e8a09c3bf050e4274a306d1aacb2b5d555fa8
David Filicko <david.filicko@gmail.com>
2025-01-18 18:18:38 +0100

    beginig of UI changes to accept locales

--- src/components/versionSelect.tsx
+++ src/components/versionSelect.tsx
@@ -1,17 +1,21 @@
-import config from "@payload-config";
-import { getPayload } from "payload";
-import { VersionSelectUi } from "./versionSelect-ui";
+import config from '@payload-config';
+import { getPayload } from 'payload';
+import { VersionSelectUi } from './versionSelect-ui';
 
 export async function VersionSelect() {
   const payload = await getPayload({ config });
   const versions = await payload.find({
-    collection: "versions",
+    collection: 'versions',
     where: {
       isVisible: { equals: true },
     },
-    sort: "-slug",
+    sort: '-slug',
+    locale: 'sk',
   });
-  // console.log(versions.docs);
+
+  console.log('verzie', versions.docs);
+
+  //tu pokracovat, pridat selektor na jazyk a nastavit do localstorage a strcit vybrany jazyk do finderov
 
   return <VersionSelectUi versions={versions.docs} />;
 }
