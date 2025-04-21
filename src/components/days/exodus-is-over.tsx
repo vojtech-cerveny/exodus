e802da3d39cd6ab8d647459959ed6c382764547d
Vojtech Cerveny <cervik49@gmail.com>
2025-04-21 21:58:02 +0200

    fix: remove old code

--- src/components/days/exodus-is-over.tsx
+++ src/components/days/exodus-is-over.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { getEventStatus } from "@/app/(app)/utils/date";
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, HomeIcon } from "@radix-ui/react-icons";
-import moment from "moment";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function ExodusIsOver() {
   const router = useRouter();
-  const exodus = getEventStatus("EXODUS");
 
   return (
     <main className="relative mt-16 flex w-full flex-col items-center justify-center">
@@ -17,10 +14,6 @@ export default function ExodusIsOver() {
       <div className="absolute top-20 rotate-12 rounded bg-[#cfcfcf] px-2 text-2xl text-zinc-800">
         Exodus už skončil / nebo ještě nezačal
       </div>
-      <p>
-        Exodus už skončil nebo ještě nezačal, tady nic nenajdeš. Zde uvidíš víc {moment(exodus.startDate).fromNow()} (
-        {moment(exodus.startDate).format("LL")})
-      </p>
       <p>Tuhle stránku si můžeš dát do svých oblíbených a vždy zde uvidíš texty na daný den.</p>
       <div className="container flex justify-center gap-4 pt-4">
         <Button onClick={() => router.back()} className="flex items-center gap-1">
