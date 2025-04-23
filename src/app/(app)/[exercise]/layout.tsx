1cf18fc860fb22a8067acbf64a2bc914a00080c7
Vojtěch Červený <12935892+vojtech-cerveny@users.noreply.github.com>
2025-04-23 10:46:07 +0200

    release/1.0.3 (hotfix) (#24)

--- src/app/(app)/[exercise]/layout.tsx
+++ src/app/(app)/[exercise]/layout.tsx
@@ -1,3 +1,3 @@
-export default function RootLayout({ children }: { children: React.ReactNode; breadcrumbs: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return <div className="mb-4 pb-8">{children}</div>;
 }
