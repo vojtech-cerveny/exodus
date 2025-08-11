caa8d0ebf730deb6cc0149ec6120080a35a1ec75
Vojtech Cerveny <cervik49@gmail.com>
2025-08-11 15:28:10 +0200

    feat: add migration for versions language support
    - Introduced a new migration to add a language column to the versions table, supporting Czech and Slovak languages.
    - Created a corresponding enum type for language options in the database.
    - Updated migration index to include the new migration for proper execution order.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,8 @@ import * as migration_20250108_213227 from "./20250108_213227";
 import * as migration_20250112_201039 from "./20250112_201039";
 import * as migration_20250203_214353 from "./20250203_214353";
 import * as migration_20250421_201656 from "./20250421_201656";
+import * as migration_20250421_201657 from "./20250421_201657";
+import * as migration_20250811_132756 from "./20250811_132756";
 
 export const migrations = [
   {
@@ -23,5 +25,15 @@ export const migrations = [
     up: migration_20250421_201656.up,
     down: migration_20250421_201656.down,
     name: "20250421_201656",
+  },
+  {
+    up: migration_20250421_201657.up,
+    down: migration_20250421_201657.down,
+    name: "20250421_201657",
+  },
+  {
+    up: migration_20250811_132756.up,
+    down: migration_20250811_132756.down,
+    name: "20250811_132756",
   },
 ];
